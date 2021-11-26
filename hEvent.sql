f9982958494aedb5774ca876807c257ba1b3f9ad
ach <82474261+ach-git@users.noreply.github.com>
2021-11-26 18:19:03 +0100

    Create hEvent.sql

---
+++ hEvent.sql
@@ -0,0 +1,13 @@
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('diamond', 'Dimamant', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('mangue', 'Mangue', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('papaye', 'Papaye', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('saumon', 'Saumon', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('hareng', 'Hareng', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('requinm', 'Requin marteau', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('requin', 'Requin', 1);
+
+
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('chargeur', 'Chargeur', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('viseur', 'Viseur', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('cross', 'Cross', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('weaponc', 'Corp d armes', 1);
